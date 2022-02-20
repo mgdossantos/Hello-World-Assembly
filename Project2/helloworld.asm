76a246c955ddabeb559896d798329927ecb734b3
Marcela Gonçalves dos Santos <marcela.santos1@uqac.ca>
2022-02-20 15:06:12 -0500

    Initial commit

---
+++ Project2/helloworld.asm
@@ -0,0 +1,26 @@
+; MASMTest.asm - add two 32-bit integers
+; Marcela dos Santos
+
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto, dwExitCode : dword
+
+
+.data
+
+sum DWORD ?
+
+
+.code
+main proc
+	mov eax,7
+	add eax,4
+	mov sum, eax
+
+	invoke ExitProcess, 0
+
+main endp
+
+end main
